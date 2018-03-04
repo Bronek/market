f5fb0355686c7fda1a033c1b3a9c8d7afe9ec340
Bronek Kozicki <brok@spamcop.net>
2018-03-04 17:22:24 +0000

    refactoring of common::emplace, added unit test

--- libs/common/utils.hpp
+++ libs/common/utils.hpp
@@ -4,38 +4,49 @@
 #include <utility>
 
 namespace common {
-    template <typename Type, bool DestructionPolicy> struct destroy_impl;
+    namespace impl {
+        template <typename Type, bool DestructionPolicy> struct destroy_impl;
 
-    template <typename Type> struct destroy_impl<Type, true> {
-        static_assert(std::is_trivially_destructible_v<Type>);
-        static void fn(Type* ) noexcept { } // no-op
-    };
+        template <typename Type> struct destroy_impl<Type, true> {
+            static_assert(std::is_trivially_destructible_v<Type>);
+            static void fn(Type* ) noexcept { } // no-op
+        };
 
-    template <typename Type> struct destroy_impl<Type, false> {
-        static void fn(Type* ptr) noexcept { ptr->~Type(); }
-    };
+        template <typename Type> struct destroy_impl<Type, false> {
+            static void fn(Type* ptr) noexcept { ptr->~Type(); }
+        };
+    }
 
     template <typename Type> struct destroy {
         using type = typename std::remove_cv<typename std::remove_reference<Type>::type>::type;
-        using impl = destroy_impl<type, std::is_trivially_destructible_v<type>>;
+        using impl = impl::destroy_impl<type, std::is_trivially_destructible_v<type>>;
         static void fn(Type* ptr) noexcept { impl::fn(ptr); }
     };
 
-    template <typename Type> struct emplace {
-        using type = typename std::remove_cv<typename std::remove_reference<Type>::type>::type;
+    namespace impl {
+        template <typename Type> struct emplace_impl {
+            using type = typename std::remove_cv<typename std::remove_reference<Type>::type>::type;
 
-        template <typename Sentinel, typename ... Args>
-        static type& fn(Sentinel* dst, Args&& ... a) noexcept {
-            using sent = typename std::remove_volatile<typename std::remove_reference<Sentinel>::type>::type;
-            static_assert(not std::is_const_v<sent>, "Overwriting of const data");
-            static_assert(std::is_same_v<sent, type>, "Unexpected pointer type, intentionally captured by overloading");
-            destroy<type>::fn(dst);
-            return *(new (dst) Type{std::forward<Args>(a)...});
-        }
+            template<typename Sentinel, typename ... Args>
+            static type* fn(Sentinel* dst, Args &&... a) noexcept {
+                destroy<type>::fn(dst); // This must fail during compilation if dst is wrong type or const
+                return new(dst) type{std::forward<Args>(a)...};
+            }
 
-        template <typename ... Args>
-        static type& fn(void* dst, Args&& ... a) noexcept {
-            return *(new (dst) Type{std::forward<Args>(a)...});
-        }
-    };
+            template <typename ... Args>
+            static type* fn(void* dst, Args&& ... a) noexcept {
+                return new (dst) type{std::forward<Args>(a)...};
+            }
+        };
+    }
+
+    template <typename Type, typename ... Args>
+    static Type* emplace(Type* dst, Args&& ... a) noexcept {
+        return impl::emplace_impl<Type>::fn(dst, std::forward<Args>(a)...);
+    }
+
+    template <typename Type, typename ... Args>
+    static Type* emplace(void* dst, Args&& ... a) noexcept {
+        return impl::emplace_impl<Type>::fn(dst, std::forward<Args>(a)...);
+    }
 } // namespace common
---
+++ tests/utils.cpp
@@ -0,0 +1,95 @@
+#include <common/utils.hpp>
+
+#include <catch2/catch.hpp>
+
+namespace {
+    struct Dummy {
+        const int i;
+
+        Dummy() : i(0) { ctor_def++; }
+        Dummy(const Dummy& s) : i(s.i) { ctor_cpy++; }
+        Dummy(Dummy&& s) : i(s.i) { ctor_mov++; }
+        Dummy(int i) : i(i) { ctor_int++; }
+        ~Dummy() { dtor++; }
+
+        static int ctor_def;
+        static int ctor_cpy;
+        static int ctor_mov;
+        static int ctor_int;
+        static int dtor;
+    };
+
+    int Dummy::ctor_def = 0;
+    int Dummy::ctor_cpy = 0;
+    int Dummy::ctor_mov = 0;
+    int Dummy::ctor_int = 0;
+    int Dummy::dtor = 0;
+}
+
+TEST_CASE("Emplace", "[emplace]") {
+    using namespace common;
+
+    // We are doing something naughty, but perfectly legal: replacing object d1 living
+    // on stack with other objects of the same type and at the same memory location
+    Dummy d1;
+
+    CHECK(Dummy::ctor_def == 1);
+    CHECK(Dummy::ctor_cpy == 0);
+    CHECK(Dummy::ctor_mov == 0);
+    CHECK(Dummy::ctor_int == 0);
+    CHECK(Dummy::dtor == 0);
+    CHECK(d1.i == 0);
+
+    CHECK(common::emplace(&d1, 2)->i == 2);
+    CHECK(Dummy::ctor_def == 1);
+    CHECK(Dummy::ctor_cpy == 0);
+    CHECK(Dummy::ctor_mov == 0);
+    CHECK(Dummy::ctor_int == 1);
+    CHECK(Dummy::dtor == 1);
+    CHECK(d1.i == 2);
+
+    CHECK(common::emplace(&d1, Dummy(3))->i == 3);
+    CHECK(Dummy::ctor_def == 1);
+    CHECK(Dummy::ctor_cpy == 0);
+    CHECK(Dummy::ctor_mov == 1);
+    CHECK(Dummy::ctor_int == 2);
+    CHECK(Dummy::dtor == 3);
+    CHECK(d1.i == 3);
+
+    {
+        Dummy tmp1;
+        CHECK(common::emplace(&d1, std::move(tmp1))->i == 0);
+        CHECK(Dummy::ctor_def == 2);
+        CHECK(Dummy::ctor_cpy == 0);
+        CHECK(Dummy::ctor_mov == 2);
+        CHECK(Dummy::ctor_int == 2);
+        CHECK(Dummy::dtor == 4);
+        CHECK(d1.i == 0);
+    }
+    CHECK(Dummy::dtor == 5);
+
+    const Dummy tmp2{5};
+    CHECK(common::emplace(&d1, tmp2)->i == 5);
+    CHECK(Dummy::ctor_def == 2);
+    CHECK(Dummy::ctor_cpy == 1);
+    CHECK(Dummy::ctor_mov == 2);
+    CHECK(Dummy::ctor_int == 3);
+    CHECK(Dummy::dtor == 6);
+    CHECK(d1.i == 5);
+
+    // We will emplace a Dummy object at the memory location of tmp3 array
+    int tmp3[4] = {};
+    auto* ptr3 = common::emplace<Dummy>((void*)tmp3, 8);
+    CHECK((void*)ptr3 == (void*)&tmp3[0]);
+    CHECK(ptr3->i == 8);
+    CHECK(Dummy::ctor_def == 2);
+    CHECK(Dummy::ctor_cpy == 1);
+    CHECK(Dummy::ctor_mov == 2);
+    CHECK(Dummy::ctor_int == 4);
+    CHECK(Dummy::dtor == 6);
+    CHECK(d1.i == 5);
+
+    // This one line is (would be, if uncommented) very naughty, because we are referring to
+    // tmp3[0] after it has been replaced by Dummy, living at the very same memory location
+    // CHECK(tmp3[0] == 8); LEAVE IT OUT EVEN THOUGH IT "WORKS" !
+}
