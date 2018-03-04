891944fb59a138d7fded7551b7610bdfe534108c
Bronek Kozicki <brok@spamcop.net>
2018-03-04 18:10:00 +0000

    added MIT license

--- libs/market/book.hpp
+++ libs/market/book.hpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2018 Bronislaw (Bronek) Kozicki
+//
+// Distributed under the MIT License. See accompanying file LICENSE
+// or copy at https://opensource.org/licenses/MIT
+
 #pragma once
 
 #include "market.hpp"
