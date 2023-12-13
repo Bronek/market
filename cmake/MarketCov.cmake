6fa4ebe9a0e504735fe74d6ed2d1387578524ec7
Bronek Kozicki <brok@incorrekt.com>
2023-12-13 19:08:48 +0000

    Prepare code coverage report

--- cmake/MarketCov.cmake
+++ cmake/MarketCov.cmake
@@ -6,9 +6,9 @@ if (CODE_COVERAGE)
     message(FATAL_ERROR "Cannot find gcovr, aborting ...")
   endif()
 
-  set (GCOVR_ADDITIONAL_ARGS --html --html-details --exclude-noncode-lines --exclude-unreachable-branches -j ${PROCESSOR_COUNT} -s)
+  set (GCOVR_ADDITIONAL_ARGS --cobertura-pretty --exclude-noncode-lines --exclude-unreachable-branches -s)
 
-  setup_target_for_coverage_gcovr_html(
+  setup_target_for_coverage_gcovr_xml(
       NAME coverage_report
       EXECUTABLE tests
       EXECUTABLE_ARGS -r console
